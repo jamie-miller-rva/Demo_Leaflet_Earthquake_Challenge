4facdb84857c7ea1b6f7cac3190d325d49b3f7f8
Jamie-RVA <47825778+jamie-miller-rva@users.noreply.github.com>
2022-01-22 10:26:24 -0500

    with USGS earthquake data

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,17 +2,7 @@
 console.log("it is working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-// another technique to create a map object 
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
-
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // title layer site see: https://docs.mapbox.com/api/maps/styles/
 // We create the tile layer that will be the background of our map.
@@ -34,37 +24,61 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
 
-//  Add a marker to the map for Los Angeles, California.
-// https://leafletjs.com/reference-1.6.0.html#circlemarker
-let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark,
+    Satellite: satelliteStreets,
+  };
 
-// Add GeoJSON data.
-let sanFranAirport =
-{
-    "type": "FeatureCollection", "features": [{
-        "type": "Feature",
-        "properties": {
-            "id": "3469",
-            "name": "San Francisco International Airport",
-            "city": "San Francisco",
-            "country": "United States",
-            "faa": "SFO",
-            "icao": "KSFO",
-            "alt": "13",
-            "tz-offset": "-8",
-            "dst": "A",
-            "tz": "America/Los_Angeles"
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [-122.375, 37.61899948120117]
-        }
-    }
-    ]
-};
+// another technique to create a map object 
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// //  Add a marker to the map for Los Angeles, California.
+// // https://leafletjs.com/reference-1.6.0.html#circlemarker
+// let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
+
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {
+//     "type": "FeatureCollection", "features": [{
+//         "type": "Feature",
+//         "properties": {
+//             "id": "3469",
+//             "name": "San Francisco International Airport",
+//             "city": "San Francisco",
+//             "country": "United States",
+//             "faa": "SFO",
+//             "icao": "KSFO",
+//             "alt": "13",
+//             "tz-offset": "-8",
+//             "dst": "A",
+//             "tz": "America/Los_Angeles"
+//         },
+//         "geometry": {
+//             "type": "Point",
+//             "coordinates": [-122.375, 37.61899948120117]
+//         }
+//     }
+//     ]
+// };
 
 
 // pointToLayer example
@@ -83,9 +97,21 @@ let sanFranAirport =
 //     }
 // }).addTo(map);
 
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function (feature, layer) {
-        layer.bindPopup("<h4>" + feature.properties.city + "</h4>" + "<hr>" + "<br>" + "<h6>faa code </h6>" +feature.properties.faa);
-    }
-}).addTo(map);
+// L.geoJSON(sanFranAirport, {
+//     onEachFeature: function (feature, layer) {
+//         layer.bindPopup("<h4>" + feature.properties.city + "</h4>" + "<hr>" + "<br>" + "<h6>faa code </h6>" +feature.properties.faa);
+//     }
+// }).addTo(map);
+
+// Retrieve the earthquake GeoJSON data from USGS site.
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Retrieve the earthquake GeoJSON data.
+d3.json(earthquakeData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
 
