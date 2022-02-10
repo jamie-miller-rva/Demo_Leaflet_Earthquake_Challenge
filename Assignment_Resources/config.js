a3efd2a977eddaaa3b81079aa39f4cf691d86043
Jamie-RVA <47825778+jamie-miller-rva@users.noreply.github.com>
2022-02-09 21:04:04 -0500

    adding working code

---
+++ Assignment_Resources/config.js
@@ -0,0 +1,2 @@
+// location of MapboxApiKey
+const API_KEY = "pk.eyJ1IjoiamFtaWUtcnZhIiwiYSI6ImNreWMzcG4wYzBsa3gycXBta3BoM2I0MncifQ.3YKvfvM1NBWX9wZkWDBI2g";
---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiamFtaWUtcnZhIiwiYSI6ImNreXFhNmt6ZTBoejYydW55YW91cjBjMDAifQ.rxErkI07pr2tTLmsPWSjuA";
